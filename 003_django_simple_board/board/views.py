eebb71bb5fcea91321218e7c9aa40c8b6a437e52
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-05-26 17:52:37 +0900

    (add) new_article_redirect

--- 003_django_simple_board/board/views.py
+++ 003_django_simple_board/board/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board
 from .forms import BoardForm
 
-def post_list(request):
+def post_create(request):
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
-            form.save()  # 폼 데이터를 데이터베이스에 저장
-            return redirect('post_list')
+            post = form.save()  # 폼 데이터를 데이터베이스에 저장
+            return redirect('post_detail', pk=post.pk)
     else:
         form = BoardForm()
-    return render(request, 'board/post_list.html', {'form': form})
+    return render(request, 'board/post_create.html', {'form': form})
 
 def post_detail(request, pk):
     post = get_object_or_404(Board, pk=pk)
--- 003_django_simple_board/config/urls.py
+++ 003_django_simple_board/config/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path
 from board import views  # board 앱의 뷰를 임포트
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.post_list, name='post_list'),  # 루트 URL에 post_list 뷰를 매핑
+    path('', views.post_create, name='post_create'),  # 루트 URL에 post_create 뷰를 매핑
     path('<int:pk>/', views.post_detail, name='post_detail'),  # 특정 게시글 조회를 위한 URL
 ]
